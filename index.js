60c1c7a6d5b5c251046304b7f151e8beae326942
Collin Jilbert <cjilbert504@gmail.com>
2020-06-11 16:32:50 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.toLowerCase().charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-' ]+/g, '');
+  }
+
+  static titleize(string) {
+    let wordsToNotTitleize = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    let stringWithFirstLetterCapitalized = Formatter.capitalize(string);
+    let splitString = stringWithFirstLetterCapitalized.split(" ");
+    let firstWord = [];
+    firstWord.push(splitString.shift());
+    splitString.forEach(e => {
+      if(wordsToNotTitleize.includes(e)) {
+        firstWord.push(e);
+      } else {
+        firstWord.push(Formatter.capitalize(e));
+      }
+    })
+    return firstWord.join(" ");
+  }
 }
