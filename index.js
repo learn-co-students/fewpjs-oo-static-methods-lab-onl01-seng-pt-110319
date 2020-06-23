b2d88365e83692e0f8f3ffddfe63b1badc881ed6
antony_sanders <antony.devon92@gmail.com>
2020-06-23 02:43:51 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 class Formatter {
-  //add static methods here
+    //add static methods here
+    static capitalize(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+
+    static sanitize(string) {
+        return string.replace(/[^A-Za-z0-9-'\s]+/g, '');
+    }
+
+    static titleize(string) {
+        let words = string.split(' ');
+        let nonTitledWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+        let titleizedWords = []
+        for (let i = 0; i < words.length; i++) {
+            if (i === 0) {
+                titleizedWords.push(this.capitalize(words[i]))
+            } else {
+                if (nonTitledWords.includes(words[i])) {
+                    titleizedWords.push(words[i])
+                } else {
+                    titleizedWords.push(this.capitalize(words[i]))
+                }
+            }
+        }
+        return titleizedWords.join(' ')
+    }
 }
