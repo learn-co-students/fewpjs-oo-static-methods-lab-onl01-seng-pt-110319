df114b2f894c3fedd5835d75d6784ac665b77ef7
stephensanchez05 <stephensanchez05@gmail.com>
2020-07-04 18:19:38 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9 '-]/g, '' );
+  }
+  static titleize(string) {
+    let answer = []
+    let badWords = ["the", 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let sentence = string.split(' ');
+    sentence.map(x => {
+    if (badWords.includes(x)){
+    answer.push(x)
+    } else {
+    
+    answer.push(this.capitalize(x))
+    }    
+  })
+    let result = answer.join(" ");
+    return this.capitalize(result)
+  }
 }
