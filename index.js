8699bfd91cdfcb7b3ed88c130373f44234ff039b
Mug <brad.alexander913@gmail.com>
2020-07-05 10:44:48 -0700

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 class Formatter {
-  //add static methods here
+  static capitalize(str){
+    const arr = str.split("")
+    let first = arr[0]
+    arr.splice(0,1,first.toUpperCase())
+    return arr.join("")
+  }
+
+  static sanitize(str){
+    return str.replace( /[^A-Za-z0-9 '-]/g, '' )
+  }
+
+  static titleize(str){
+    let exempt = ["this","a","an","but","of","and","for","at","by","from","the"]
+    let words = str.split(" ")
+    let result = []
+    for (let word of words){
+      if (exempt.includes(word)){
+        result.push(word)
+      }
+      else {
+        result.push( this.capitalize(word))
+      }
+    }
+    result.splice(0,1,this.capitalize(result[0]))
+    return result.join(" ");
+  }
+
 }
