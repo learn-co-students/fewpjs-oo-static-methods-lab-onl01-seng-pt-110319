ec76ce347a745357e43593688d5680efe76d4350
kchohee <kchohee0630@gmail.com>
2020-07-07 19:28:31 -0500

    Done.

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ class Formatter {
     return string.charAt(0).toUpperCase()+ string.slice(1)
   }
   static sanitize(string){ 
-    return string.replace(/[^A-Za-z0-9 -']/g,'');
+    return string.replace(/[^A-Za-z0-9 '-]/g,'');
   }
   static titleize(string){
-    let words = string.split(" ")
+    let cleanString = string.replace(/[^A-Za-z0-9 '-]/g,'')
+    let cleanCap = cleanString.charAt(0).toUpperCase()+ string.slice(1)
+    let words = cleanCap.split(" ")
     let ignore = ["this","a","an","but","of","and","for","at","by","from","the"]
     let title = []
     for (let word of words){
